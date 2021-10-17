7c171001b07707a0c511058502a98df72dc7a726
Tayyab Ferozi <ferozitayyab@gmail.com>
2021-10-17 18:37:34 +0500

    final fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,40 +13,54 @@ const Navbar = () => {
   };
 
   return (
-    <nav id="navbar">
-      <div className={`sidenav${isSideNavActive ? " active" : ""}`}>
-        <div id="close" onClick={toggleSideBarHandler}>
-          &times;
+    <>
+      <div className={`dark-overlay ${isSideNavActive ? " active" : ""}`}></div>
+      <nav id="navbar">
+        <div className={`sidenav${isSideNavActive ? " active" : ""}`}>
+          <div id="close" onClick={toggleSideBarHandler}>
+            &times;
+          </div>
+          <ul className="nav-sm">
+            <li onClick={toggleSideBarHandler}>
+              <Link to="/">Dashboard</Link>
+            </li>
+            <li onClick={toggleSideBarHandler}>
+              <Link to="/watchlist">Watchlist</Link>
+            </li>
+            <li onClick={toggleSideBarHandler}>
+              <Link to="/trade-journal">Trade Journal</Link>
+            </li>
+          </ul>
         </div>
-      </div>
-      <div>
-        <img
-          onClick={toggleSideBarHandler}
-          className="menu"
-          src={Hamburger}
-          alt="menu"
-        />
-      </div>
-      <ul class="nav">
-        <li>
-          <Link to="/">Dashboard</Link>
-        </li>
-        <li>
-          <Link to="watchlist">Watchlist</Link>
-        </li>
-        <li>
-          <Link to="/trade-journal">Trade Journal</Link>
-        </li>
-      </ul>
-      <div class="btns-container">
-        <a class="btn" href>
-          JOIN NOW
-        </a>
-        <a class="btn" href>
-          LOGIN
-        </a>
-      </div>
-    </nav>
+        <div>
+          <img
+            onClick={toggleSideBarHandler}
+            className="menu"
+            src={Hamburger}
+            alt="menu"
+          />
+        </div>
+        <ul className="nav">
+          <li>
+            <Link to="/">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/watchlist">Watchlist</Link>
+          </li>
+          <li>
+            <Link to="/trade-journal">Trade Journal</Link>
+          </li>
+        </ul>
+        <div className="btns-container">
+          <a className="btn" href>
+            JOIN NOW
+          </a>
+          <a className="btn" href>
+            LOGIN
+          </a>
+        </div>
+      </nav>
+    </>
   );
 };
 
